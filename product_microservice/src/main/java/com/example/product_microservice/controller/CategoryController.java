1ca1f7d36c0e7f067ccbce6e9758d3a769372f2c
jptolozac <jeanpaultoloza@gmail.com>
2024-12-16 21:24:59 -0500

    Add: update scopes from config server

--- product_microservice/src/main/java/com/example/product_microservice/controller/CategoryController.java
+++ product_microservice/src/main/java/com/example/product_microservice/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.example.product_microservice.controller;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/categories")
 @Getter
 @Setter
+@RefreshScope
 public class CategoryController {
 
     @Value("${app.testProp}")
